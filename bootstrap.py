6b98c34f362945a7bfd7c93b910096aa85c92fa7
Nick Hirakawa <nickhirakawa@gmail.com>
2018-01-01 15:33:02 -0500

    python3 compatible

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import argparse
 import json
 import os
@@ -19,7 +21,7 @@ def main():
     if backup_extension.startswith('.'):
         backup_extension = backup_extension.split('.')[1]
 
-    print 'installing dotfiles to', USER_DIR
+    print('installing dotfiles to', USER_DIR)
 
     index = read_index()
 
@@ -32,30 +34,30 @@ def main():
 
     for dotfile in dotfiles:
         if 'source' not in dotfile:
-            print 'field `source` not found in {} - skipping'.format(dotfile)
+            print('field `source` not found in {} - skipping'.format(dotfile))
             continue
 
         if "target" not in dotfile:
-            print 'field `target` not found in {} - skipping'.format(dotfile)
+            print('field `target` not found in {} - skipping'.format(dotfile))
             continue
 
         source = path.join(CONF_DIR, dotfile['source'])
         target = path.join(USER_DIR, dotfile['target'])
 
-        print
-        print 'installing {}'.format(source)
+        print()
+        print('installing {}'.format(source))
 
         directory_name = path.dirname(target)
         if not path.exists(directory_name):
-            print '  creating directory {}'.format(directory_name)
+            print('  creating directory {}'.format(directory_name))
             os.makedirs(directory_name)
 
         if path.lexists(target):
             backup_name = '{}.{}.{}'.format(target, now, backup_extension)
-            print '  backing up {} to {}'.format(target, backup_name)
+            print('  backing up {} to {}'.format(target, backup_name))
             os.rename(target, backup_name)
 
-        print '  linking {} to {}'.format(source, target)
+        print('  linking {} to {}'.format(source, target))
         os.symlink(source, target)
 
 
